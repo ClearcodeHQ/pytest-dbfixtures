32a0dfe5d2957cdcee7b951d71a1ba61ad03077e
Tomasz Święcicki <tomislater@gmail.com>
2014-01-25 23:30:11 +0100

    Add docs for redis

--- pytest_dbfixtures/factories.py
+++ pytest_dbfixtures/factories.py
@@ -7,9 +7,29 @@ from pytest_dbfixtures.utils import get_config, try_import
 
 def redis_proc(executable=None, params=None, config_file=None,
                host=None, port=None):
+    """
+    Redis process factory.
+
+    :param str executable: path to redis-server
+    :param str params: params
+    :param str config_file: path to config file
+    :param str host: hostname
+    :param str port: port
+    :rtype: func
+    :returns: function which makes a redis process
+    """
 
     @pytest.fixture(scope='session')
     def redis_proc_fixture(request):
+        """
+        #. Get configs.
+        #. Run redis process.
+        #. Stop redis process after tests.
+
+        :param FixtureRequest request: fixture request object
+        :rtype: summon_process.executors.tcp_coordinated_executor.TCPCoordinatedExecutor
+        :returns: tcp executor
+        """
         config = get_config(request)
 
         redis_exec = executable or config.redis.redis_exec
@@ -49,10 +69,29 @@ def redis_proc(executable=None, params=None, config_file=None,
 
 
 def redisdb(process_fixture_name, host=None, port=None, db=None):
+    """
+    Redis database factory.
+
+    :param str process_fixture_name: name of the process fixture
+    :param str host: hostname
+    :param int port: port
+    :param int db: number of database
+    :rtype: func
+    :returns: function which makes a connection to redis
+    """
 
     @pytest.fixture
     def redisdb_factory(request):
-        # load required process fixture
+        """
+        #. Load required process fixture.
+        #. Get redis module and config.
+        #. Connect to redis.
+        #. Flush database after tests.
+
+        :param FixtureRequest request: fixture request object
+        :rtype: redis.client.Redis
+        :returns: Redis client
+        """
         request.getfuncargvalue(process_fixture_name)
 
         redis, config = try_import('redis', request)
@@ -63,6 +102,7 @@ def redisdb(process_fixture_name, host=None, port=None, db=None):
 
         redis_client = redis.Redis(redis_host, redis_port, redis_db)
         request.addfinalizer(redis_client.flushall)
+
         return redis_client
 
     return redisdb_factory
