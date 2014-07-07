e1d2bc36b0e8776a705eab1e5a7488d39067b582
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-07-07 20:47:47 +0200

    bump mirakuru to the newest version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pytest>=2.3.4',
-        'mirakuru>=0.1.0',
+        'mirakuru>=0.1.2',
         'pyaml>=3.10',
         'pymlconf>=0.2.10a',
         'path.py>=4.2',
