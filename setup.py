100fca102c2debd83d8ac40f78c69904bf0a8c08
pwilczynskiclearcode <p.wilczynski@clearcode.cc>
2014-11-08 15:09:53 +0100

    Added missing package classifiers

--- setup.py
+++ setup.py
@@ -92,7 +92,10 @@ setup(
         'Topic :: Software Development :: Testing',
         'Topic :: Utilities',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ]
