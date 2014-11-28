e389b668ead4bc995643d700a1d5edc2900ad466
Paweł Wilczyński <p.wilczynski@clearcode.cc>
2014-11-28 09:24:25 +0100

    Release 0.8.1

--- pytest_dbfixtures/__init__.py
+++ pytest_dbfixtures/__init__.py
@@ -16,4 +16,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytest-dbfixtures. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
